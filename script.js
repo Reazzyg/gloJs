45e4063d1f194c4fd8a96dbdb4cd7f6b1b45f761
Semion Mudrov <mudrsemen@ya.ru>
2025-01-09 22:59:15 +0300

    Feat: created variables

---
+++ script.js
@@ -0,0 +1,9 @@
+let title;
+let screens;
+let screenPrice;
+let rollback;
+let fullPrice;
+let adaptive;
+
+alert('Добро пожаловать в наш магазин!');
+console.log('Добро пожаловать в наш магазин!');
