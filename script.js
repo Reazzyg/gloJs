adbd90a38c4f50cd63f99a29655a33d82c767523
Semion Mudrov <mudrsemen@ya.ru>
2025-01-13 22:41:01 +0300

    Update: added user interaction & price calculation

--- script.js
+++ script.js
@@ -1,19 +1,45 @@
-let title = '';
-let screens = 'Простые, Сложные, Интерактивные';
-let screenPrice = 3;
-let rollback = 43;
-let fullPrice = 50000;
-let adaptive = true;
+'use strict';
 
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
-console.log(screens.length);
-console.log(
-  `Стоимость верстки экранов ${screenPrice} рублей/долларов/гривен/юани`,
-);
-console.log(
-  `Стоимость разработки сайта ${fullPrice} рублей/долларов/гривен/юани`,
-);
-console.log(screens.toLowerCase().split(', '));
-console.log(fullPrice * (rollback / 100));
+let title = prompt('Как называется ваш проект?');
+
+let screens = prompt('Какие типы экранов нужно разработать?');
+
+let screenPrice = Number(prompt('Сколько будет стоить данная работа?'));
+
+let adaptive = confirm('Нужен ли адаптив на сайте?');
+
+let service1 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice1 = Number(prompt('Сколько это будет стоить?'));
+
+let service2 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice2 = Number(prompt('Сколько это будет стоить?'));
+
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+
+let rollback = 43;
+
+let servicePercentPrice = Math.ceil(fullPrice - fullPrice * (rollback / 100));
+console.log(servicePercentPrice);
+
+if (fullPrice >= 30000) {
+  console.log('Даем скидку в 10%');
+} else if (fullPrice >= 15000 && fullPrice < 30000) {
+  console.log('Даем скидку в 5%');
+} else if (fullPrice < 15000) {
+  console.log('Скидка не предусмотрена');
+} else {
+  console.log('Что то пошло не так');
+}
+
+// console.log(typeof title);
+// console.log(typeof fullPrice);
+// console.log(typeof adaptive);
+// console.log(screens.length);
+// console.log(
+//   `Стоимость верстки экранов ${screenPrice} рублей/долларов/гривен/юани`,
+// );
+// console.log(
+//   `Стоимость разработки сайта ${fullPrice} рублей/долларов/гривен/юани`,
+// );
+// console.log(screens.toLowerCase().split(', '));
+// console.log(fullPrice * (rollback / 100));
