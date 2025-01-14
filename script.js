818445a9be21212ab7d68e8cdd8b920475ef2fd4
Semion Mudrov <mudrsemen@ya.ru>
2025-01-14 13:27:37 +0300

    Feat: lesson 4 homework

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 'use strict';
 
 let title = prompt('Как называется ваш проект?');
@@ -43,3 +44,24 @@ if (fullPrice >= 30000) {
 // );
 // console.log(screens.toLowerCase().split(', '));
 // console.log(fullPrice * (rollback / 100));
+=======
+let title = '';
+let screens = 'Простые, Сложные, Интерактивные';
+let screenPrice = 3;
+let rollback = 43;
+let fullPrice = 50000;
+let adaptive = true;
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log(
+  `Стоимость верстки экранов ${screenPrice} рублей/долларов/гривен/юани`,
+);
+console.log(
+  `Стоимость разработки сайта ${fullPrice} рублей/долларов/гривен/юани`,
+);
+console.log(screens.toLowerCase().split(', '));
+console.log(fullPrice * (rollback / 100));
+>>>>>>> Stashed changes
